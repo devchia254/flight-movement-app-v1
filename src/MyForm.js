79aa2f1a66ab07e89e4a272fc569f5daf286b0c5
alex <alex7chiaster@gmail.com>
2020-05-23 01:03:07 +0800

    added yup validation to all fields except dateTime

--- src/MyForm.js
+++ src/MyForm.js
@@ -3,6 +3,15 @@ import { Button } from "@material-ui/core";
 import { Formik, Form, Field } from "formik";
 import MyField from "./MyField.js";
 import MyKBDateTimePicker from "./MyKBDateTimePicker.js";
+import * as yup from "yup";
+
+const yupValidationSchema = yup.object({
+  flightNo: yup.string().required().max(10),
+  acReg: yup.string().required().max(10),
+  from: yup.string().required().max(10),
+  to: yup.string().required().max(10),
+  company: yup.string().required().max(10),
+});
 
 function MyForm({ onSubmit }) {
   return (
@@ -15,9 +24,7 @@ function MyForm({ onSubmit }) {
         to: "",
         company: "",
       }}
-      nextInitialState={{
-        dateTime: "",
-      }}
+      validationSchema={yupValidationSchema}
       onSubmit={(values, { resetForm }) => {
         onSubmit(values);
         // Clear form after submit
@@ -42,25 +49,27 @@ function MyForm({ onSubmit }) {
               component={MyKBDateTimePicker}
             />
           </div>
+
           <div>
-            <Field
+            <MyField label="Flight No." name="flightNo" id="jjbkjb" />
+            {/* <Field
               label="Flight No."
               name="flightNo"
               id="jjbkjb"
               component={MyField}
-            />
+            /> */}
           </div>
           <div>
-            <Field label="Aircraft Reg." name="acReg" component={MyField} />
+            <MyField label="Aircraft Reg." name="acReg" />
           </div>
           <div>
-            <Field label="From" name="from" component={MyField} />
+            <MyField label="From" name="from" />
           </div>
           <div>
-            <Field label="To" name="to" component={MyField} />
+            <MyField label="To" name="to" />
           </div>
           <div>
-            <Field label="Company" name="company" component={MyField} />
+            <MyField label="Company" name="company" />
           </div>
           <Button type="submit">submit</Button>
           <pre>{JSON.stringify({ errors, values }, null, 2)}</pre>
