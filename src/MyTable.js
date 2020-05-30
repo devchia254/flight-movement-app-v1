743562417f7c7d79d79ed82d5692ef021bae4b0e
alex <alex7chiaster@gmail.com>
2020-05-31 00:07:00 +0800

    Added icons, finally

--- src/MyTable.js
+++ src/MyTable.js
@@ -1,47 +1,124 @@
 import React from "react";
 // import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+// import Table from "@material-ui/core/Table";
+// import TableBody from "@material-ui/core/TableBody";
+// import TableCell from "@material-ui/core/TableCell";
+// import TableContainer from "@material-ui/core/TableContainer";
+// import TableHead from "@material-ui/core/TableHead";
+// import TableRow from "@material-ui/core/TableRow";
+// import Paper from "@material-ui/core/Paper";
+import MaterialTable from "material-table";
+
+import { forwardRef } from "react";
+
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
 
 function MyTable({ flights }) {
   // const { flights } = props;
+  console.log(flights[0].acReg);
+
+  const columns = [
+    { title: "ID", field: "id" },
+    { title: "Flight No.", field: "flightNo" },
+    { title: "Aircraft Reg.", field: "acReg", type: "numeric" },
+    { title: "Date", field: "date" },
+    { title: "Time", field: "time" },
+    { title: "From", field: "from", type: "numeric" },
+    { title: "To", field: "to" },
+    { title: "Company", field: "company" },
+  ];
+
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Flight No.</TableCell>
-            <TableCell>Aircraft Reg.</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Time</TableCell>
-            <TableCell>From</TableCell>
-            <TableCell>To</TableCell>
-            <TableCell>Company</TableCell>
-            <TableCell>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {flights.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.flightNo}</TableCell>
-              <TableCell>{row.acReg}</TableCell>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.time}</TableCell>
-              <TableCell>{row.from}</TableCell>
-              <TableCell>{row.to}</TableCell>
-              <TableCell>{row.company}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <MaterialTable
+      icons={tableIcons}
+      title="Conditional Actions Preview"
+      columns={columns}
+      data={flights}
+      actions={[
+        {
+          icon: () => <SaveAlt />,
+          tooltip: "Save User",
+          onClick: (event, rowData) => alert("You saved " + rowData.name),
+        },
+        (rowData) => ({
+          icon: () => <DeleteOutline />,
+          tooltip: "Delete User",
+          onClick: (event, rowData) =>
+            alert("You want to delete " + rowData.name),
+          disabled: rowData.birthYear < 2000,
+        }),
+      ]}
+    />
   );
 }
 export default MyTable;
+
+// <TableContainer component={Paper}>
+//   <Table aria-label="simple table">
+//     <TableHead>
+//       <TableRow>
+//         <TableCell>ID</TableCell>
+//         <TableCell>Flight No.</TableCell>
+//         <TableCell>Aircraft Reg.</TableCell>
+//         <TableCell>Date</TableCell>
+//         <TableCell>Time</TableCell>
+//         <TableCell>From</TableCell>
+//         <TableCell>To</TableCell>
+//         <TableCell>Company</TableCell>
+//         <TableCell>Action</TableCell>
+//       </TableRow>
+//     </TableHead>
+//     <TableBody>
+//       {flights.map((row) => (
+//         <TableRow key={row.id}>
+//           <TableCell>{row.id}</TableCell>
+//           <TableCell>{row.flightNo}</TableCell>
+//           <TableCell>{row.acReg}</TableCell>
+//           <TableCell>{row.date}</TableCell>
+//           <TableCell>{row.time}</TableCell>
+//           <TableCell>{row.from}</TableCell>
+//           <TableCell>{row.to}</TableCell>
+//           <TableCell>{row.company}</TableCell>
+//         </TableRow>
+//       ))}
+//     </TableBody>
+//   </Table>
+// </TableContainer>
