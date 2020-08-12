2ef2c4ba066af571c2e765bdfc38daea9eb7437d
devchia254 <alex7chiaster@gmail.com>
2020-08-12 11:10:50 +0800

    Edited table and added a grid to accomodate table and future weather component

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Homepage() {
-  return (
-    <div>
-      <h1>This is the homepage route!</h1>
-    </div>
-  );
+import makeData from "../test/makeData"; // Fake data generator
+import HomeTable from "../components/table/HomeTable";
+
+import { Container } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = (theme) => ({
+  container: {
+    marginTop: theme.spacing(3),
+  },
+});
+
+class Homepage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      flights: [...makeData(3)],
+    };
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Container maxWidth="lg">
+        <Grid container spacing={3} className={classes.container}>
+          <Grid item xs={12} sm={8}>
+            <HomeTable flights={this.state.flights} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper>Weather App</Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  }
 }
+
+export default withStyles(useStyles)(Homepage);
